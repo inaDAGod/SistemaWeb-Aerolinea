fde4add5cf7006595f82917d640b07f837c7b0e7
luzticona7 <chaconlu68@gmail.com>
2024-04-18 11:30:01 -0400

    pagina principal lista1

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ window.addEventListener('click', e=>{
 
     }
 })
+
+var swiper= new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 80,
+    grabCursor: true,
+    loop:true,
+    breakpoints:{
+        991:{
+            slidesPerView:3
+        }
+    }
+})
