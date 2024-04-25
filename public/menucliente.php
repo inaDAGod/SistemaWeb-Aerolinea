0d895e6ee00c0c905a6a60b7bb995e17390abacb
unknown <ayana.siegle@ucb.edu.bo>
2024-04-24 20:39:00 -0400

    cambios del nav
    cambie los otros 2

--- public/menucliente.php
+++ public/menucliente.php
@@ -17,18 +17,21 @@
                 <ul class="navbar-nav" style="display: flex; align-items: center;position: absolute; left: 0;margin-right:10%;">
                     <li class="nav-item">
                         <div style="display: flex; align-items: center;margin-right: 10px;">
-                            <img src="assets\avion.png" alt="Menu Icon" style="width:70%;">
-                            <h3>VuelaBodo</h3>
+                            <img src="assets\logoavion.png" alt="Menu Icon" style="width: 150px; height: 50%;margin-left:10px;margin-top: 10px; margin-bottom: 20px;">
+                            
                         </div>
                     </li>
+                    
+
+                    
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php" style="margin-left: 130%;top: 2%;">Home</a>
+                        <a class="nav-link" href="servicios.php" style="margin-left: 780%;top: 2%;">Vuelos</a> <!-- Removed inline styles -->
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="servicios.php" style="margin-left: 130%;top: 2%;">Vuelos</a>
+                        <a class="nav-link" href="Login.php" style="margin-left: 850%;top: 2%;width:150%;">Check in</a> <!-- Removed inline styles -->
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="Login.php" style="margin-left: 130%;top: 2%;width:200%;">Check in</a>
+                        <a class="nav-link" href="servicios.php" style="margin-left: 730%;top: 2%;width:200%;">Premios Millas</a> <!-- Removed inline styles -->
                     </li>
                 </ul>
             </nav>
--- public/menusuperadmin.php
+++ public/menusuperadmin.php
@@ -10,28 +10,28 @@
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-<header class="py-3">
+<header class="py-3"> 
     <div class="container d-flex justify-content-between align-items-center"style=" padding: 20px; margin-top: 15px; margin-bottom: 5px;color:white;">
     <nav class="navbar navbar-expand-lg navbar" style="position: absolute; left: 0; margin-bottom: 5px;">
 
                 <ul class="navbar-nav" style="display: flex; align-items: center;position: absolute; left: 0;margin-right:10%;">
                     <li class="nav-item">
                         <div style="display: flex; align-items: center;margin-right: 10px;">
-                            <img src="assets\avion.png" alt="Menu Icon" style="width:70%;">
-                            <h3>VuelaBodo</h3>
+                            <img src="assets\logoavion.png" alt="Menu Icon" style="width: 150px; height: 50%;margin-left:10px;margin-top: 10px; margin-bottom: 20px;">
+                            
                         </div>
                     </li>
+                    
+
+                    
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php" style="font-size: 25px;color:white;margin-left: 100%;top: 2%;">Home</a>
+                        <a class="nav-link" href="agregarVuelo.php" style="margin-left: 800%;top: 2%;">Vuelos</a> <!-- Removed inline styles -->
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="servicios.php" style="font-size: 25px;color:white;margin-left: 100%;">Vuelos</a>
+                        <a class="nav-link" href="" style="margin-left: 630%;top: 2%;width:150%;">Reservas</a> <!-- Removed inline styles -->
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="Login.php" style="font-size: 25px;color:white;width:150%;margin-left: 100%;">Reserva</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Login.php" style="font-size: 25px;color:white;width:150%;margin-left: 100%;">Dashboard</a>
+                        <a class="nav-link" href="servicios.php" style="margin-left: 580%;top: 2%;width:200%;">Dashboard</a> <!-- Removed inline styles -->
                     </li>
                 </ul>
             </nav>
