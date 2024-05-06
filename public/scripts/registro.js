feb9d3935d25e51fde577be6046cdb21070d2e97
inaDAGod <danialee14@gmail.com>
2024-05-06 19:14:42 -0400

    add frontend olvidarse contraseña

--- public/scripts/registro.js
+++ public/scripts/registro.js
@@ -15,15 +15,29 @@ function toggleForm() {
 }
 
 function toggleFormVeri() {
-  let nombres = document.getElementById("nombre").value;
-  let apellidos = document.getElementById("apellido").value;
-  let correo = document.getElementById("email").value;
-  let contrasenia = document.getElementById("contra").value;
-
       var registroForm = document.getElementById('registro');
-      var inicioSesionForm = document.getElementById('inicio-sesion');
       var veriForm = document.getElementById('verificacion');
       registroForm.style.display = 'none';
-      inicioSesionForm.style.display = 'none';
       veriForm.style.display= 'block';
+}
+function toggleFormRes(){
+  var inicioSesionForm = document.getElementById('inicio-sesion');
+  var resForm = document.getElementById('restauracion');
+  resForm.style.display= 'block';
+  inicioSesionForm.style.display = 'none';
+ 
+}
+function toggleFormRes2(){
+  var resForm = document.getElementById('restauracion2');
+  var resForm2 = document.getElementById('restaurarContra');
+  resForm2.style.display= 'block';
+  resForm.style.display = 'none';
+ 
+}
+function toggleFormRes3(){
+  var resForm = document.getElementById('restauracion');
+  var resForm2 = document.getElementById('restauracion2');
+  resForm2.style.display= 'block';
+  resForm.style.display = 'none';
+ 
 }
