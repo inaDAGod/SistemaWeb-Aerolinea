549aa40be0bf2533fdc990dcf96b46822682b03c
inaDAGod <danialee14@gmail.com>
2024-05-08 11:45:54 -0400

    guarda en la bd el producto, ademas guarda imagen

---
+++ backend/addPremio.php
@@ -0,0 +1,53 @@
+<?php
+// Verificar si se han enviado archivos
+if ($_FILES['foto']) {
+    // Obtener los datos del formulario
+    $premio = $_POST['premio'];
+    $millas = $_POST['millas'];
+    $destacado = $_POST['destacado'];
+    $tipoProducto = $_POST['tipoProducto'];
+
+    // Procesar el archivo de imagen
+    if ($_FILES['foto']['error'] === UPLOAD_ERR_OK) {
+        $nombre_archivo = $_FILES['foto']['name'];
+        $ruta_temporal = $_FILES['foto']['tmp_name'];
+        $ruta_destino = "../public/assets/" . $nombre_archivo;
+        $src_foto = "/assets/".$nombre_archivo;
+        // Mover el archivo a su ubicación final
+        if (move_uploaded_file($ruta_temporal, $ruta_destino)) {
+            // Conectar a la base de datos
+            $conexion = pg_connect("dbname=aerolinea user=postgres password=admin");
+            if (!$conexion) {
+                die("Error al conectar a la base de datos: " . pg_last_error());
+            }
+
+            // Insertar los datos en la base de datos
+            $sql = "INSERT INTO premios_millas (premio, tipo_premio, producto_destacado, millas, src_foto) VALUES ('$premio', '$tipoProducto', $destacado, $millas, '$src_foto')";
+            $resultado = pg_query($conexion, $sql);
+            
+            if ($resultado) {
+                $response = array('estado' => 'registro_exitoso');
+                echo json_encode($response);
+            } else {
+                $response = array('estado' => 'error_registro');
+                echo json_encode($response);
+            }
+
+            // Cerrar la conexión
+            pg_close($conexion);
+        } else {
+            // Error al mover el archivo
+            $response = array('estado' => 'error_archivo');
+            echo json_encode($response);
+        }
+    } else {
+        // Error en la carga del archivo
+        $response = array('estado' => 'error_carga_archivo');
+        echo json_encode($response);
+    }
+} else {
+    // Error: no se han enviado archivos
+    $response = array('estado' => 'error_no_archivo');
+    echo json_encode($response);
+}
+?>
--- public/scripts/addPremio.js
+++ public/scripts/addPremio.js
@@ -3,21 +3,43 @@ function guardarProducto(){
     let millas = document.getElementById("millas").value;
     let destacado = document.querySelector('input[name="destacadosOption"]:checked').value;
     let tipoProducto = document.getElementById("tipo").value;
-    if(nombre && millas && destacado && tipoProducto){
-        if(destacado == "destacado"){
-            destacado = true;
+    let formData = new FormData();
+    let nombre_archivo = document.getElementById("foto");
+    if(nombre && millas && destacado && tipoProducto && nombre_archivo){
+        if(destacado == 'destacado'){
+            destacado = "true";
         }
         else{
-            destacado = false;
+            destacado = "false";
         }
-        //console.log(nombre); 
-        //console.log(millas);
-        //console.log(destacado);
-        //console.log(tipoProducto);
+        formData.append('premio', nombre);
+        formData.append('millas', millas);
+        formData.append('destacado', destacado);
+        formData.append('tipoProducto', tipoProducto);
+        formData.append('foto', nombre_archivo.files[0]); // Agregar el archivo al FormData
+        fetch("http://localhost/SistemaWeb-Aerolinea/backend/addPremio.php", {
+                method: "POST",
+                body: formData, // Enviar el FormData en lugar de JSON.stringify()
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Error en la solicitud');
+            })
+            .then(data => {
+                if (data.estado === "registro_exitoso") {
+                    window.location.href= 'http://localhost/SistemaWeb-Aerolinea/public/addProducto.html';
+                } else if (data.estado === "error_registro") {
+                    alert('Ya se añadio un producto igual');
+                }
+            })
+            .catch(error => {
+                console.error('Error en la solicitud:', error);
+                alert('Parece que hubo un error en la solicitud vuelve a intentar mas tarde');
+            });
     }
     else{
         alert('Llena todos los campos');
     }
-   
-
 }
