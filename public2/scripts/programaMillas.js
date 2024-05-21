2984b3dd9d1969088c502fc2b834df2004792424
inaDAGod <danialee14@gmail.com>
2024-05-20 20:51:52 -0400

    fix main

--- public2/scripts/programaMillas.js
+++
@@ -1,9 +0,0 @@
-function programaMillas(correoUsuario){
-    fetch("http://localhost/SistemaWeb-Aerolinea/backend/programaMillas.php", {
-        method: "POST",
-        body: JSON.stringify({ correo: correoUsuario }),
-    })
-    .catch(error => {
-        console.error('Error en la solicitud:', error);
-    });
-}
