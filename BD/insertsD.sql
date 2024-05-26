79486e609abb6575f31b95c59a7da87df3d3e3fd
inaDAGod <danialee14@gmail.com>
2024-05-26 11:35:07 -0400

    inserts premios

---
+++ BD/insertsD.sql
@@ -0,0 +1,12 @@
+INSERT INTO premios_millas (premio, tipo_premio, producto_destacado, millas, src_foto) 
+VALUES 
+('Viaje a Santa Cruz', 'Viaje', true, 50000, '/SistemaWeb-Aerolinea/public/assets/santa.webp'),
+('Reloj de Lujo', 'Producto', false, 25000, '/SistemaWeb-Aerolinea/public/assets/reloj.webp'),
+('Cena Gourmet', 'Comida', false, 10000, '/SistemaWeb-Aerolinea/public/assets/cena.webp'),
+('Auriculares Inalámbricos', 'Producto', true, 15000, ''),
+('Estancia en Hotel 5 Estrellas', 'Experiencia', true, 30000, ''),
+('Bicicleta de Montaña', 'Producto', false, 20000, ''),
+('Spa de Lujo', 'Cuidado Personal', false, 12000, ''),
+('Televisor 4K', 'Producto', true, 35000, ''),
+('Entradas para Concierto', 'Otro', false, 8000, ''),
+('Maleta de Viaje', 'Producto', false, 7000, '');
