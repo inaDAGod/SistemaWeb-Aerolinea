b51161ddf86fdcbc09f28d209d89a7141f838994
JoshNisth <joshnisth@gmail.com>
2024-05-26 20:10:02 -0400

    arreglo de bug de un solo gmail

--- backend/actualizarCheckin.php
+++ backend/actualizarCheckin.php
@@ -7,7 +7,6 @@ if (!$conexion) {
     exit;
 }
 
-$email = $_POST['email'] ?? '';
 $carnet = $_POST['carnet'] ?? '';
 $numeroVuelo = $_POST['numeroVuelo'] ?? '';
 $equipajeMano = $_POST['equipajeMano'] === 'true' ? 'true' : 'false'; // Asegurarse de que el valor es estrictamente 'true' o 'false'
@@ -15,14 +14,13 @@ $maleta = $_POST['maleta'] === 'true' ? 'true' : 'false';
 $equipajeExtra = $_POST['equipajeExtra'] === 'true' ? 'true' : 'false';
 
 $query = "UPDATE check_in SET 
-            correo_usuario = $1, 
             estado_checkin = 'Realizado', 
-            equipaje_mano = $2, 
-            maleta = $3, 
-            equipaje_extra = $4 
-          WHERE ccheck_in = (SELECT ccheck_in FROM boletos WHERE ci_persona = $5 AND cvuelo = $6)";
+            equipaje_mano = $1, 
+            maleta = $2, 
+            equipaje_extra = $3 
+          WHERE ccheck_in = (SELECT ccheck_in FROM boletos WHERE ci_persona = $4 AND cvuelo = $5)";
 $result = pg_prepare($conexion, "update_checkin", $query);
-$result = pg_execute($conexion, "update_checkin", array($email, $equipajeMano, $maleta, $equipajeExtra, $carnet, $numeroVuelo));
+$result = pg_execute($conexion, "update_checkin", array($equipajeMano, $maleta, $equipajeExtra, $carnet, $numeroVuelo));
 
 if ($result && pg_affected_rows($result) > 0) {
     echo json_encode(['success' => true]);
