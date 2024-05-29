b6a3e5635bdbb7dbe98e268e9ef09636918fd6e9
unknown <ayana.siegle@ucb.edu.bo>
2024-05-29 04:09:27 -0400

    movida de js y css

--- public/scripts/scriptre.js
+++ public/scripts/scriptre.js
@@ -13,22 +13,3 @@ $(document).ready(function() {
     });
 });
 
-
-
-document.getElementById('eliminar-reserva-btn').addEventListener('click', function() {
-    fetch('http://localhost/SistemaWeb-Aerolinea/backend/eliminar_reserva.php', {
-        method: 'POST'
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            alert("Error: " + data.error);
-        } else {
-            alert(data.message);
-            window.location.href = 'indexCliente.html';
-        }
-    })
-    .catch(error => {
-        console.error('Error eliminando reserva:', error);
-    });
-});
