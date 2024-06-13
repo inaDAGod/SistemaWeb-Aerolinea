6e28b9fb482772a4cdd9d8c09b457d33b003f45d
inaDAGod <danialee14@gmail.com>
2024-06-13 12:34:36 -0400

    login y register

--- Flutter_app2/satisfacion/lib/main.dart
+++ Flutter_app2/satisfacion/lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'Satisform.dart'; // Importa el archivo formsati.dart
+import 'login/auth_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AuthPage(), // Use your TaskListScreen widget as the home screen
+    );
+  }
 }
